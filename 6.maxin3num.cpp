32d2c9320b597417074a20002b5184a6f08919de
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2024-05-04 21:14:44 +0530

    Add files via upload

---
+++ 6.maxin3num.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//    or
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+//	int a, b;
+//	a = 10;
+//	b = 20;
+//	cout << max(a, b);
+	int a, b, c;
+	a = 10;
+	b = 20;
+	c = 30;
+	cout << max(a, max(b, c));
+}
